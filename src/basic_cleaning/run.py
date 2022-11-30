0021b061a03c4fde9bf2ff33fb8c4b875375eeba
cpjosejesus <cp.josejesus@gmail.com>
2022-11-29 22:16:19 -0400

    added boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,9 @@ def go(args):
     idx = df["price"].between(min_price, max_price)
     df = df[idx].copy()
 
+    idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("SAVE Clean artifact")
     df.to_csv(args.output_artifact, index=False)
 
